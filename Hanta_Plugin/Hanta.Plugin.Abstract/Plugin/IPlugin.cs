97e43c1c874cfbb401c770bc1b6167251d71658a
KR-JasonLane <malbox5034@naver.com>
2024-05-29 21:56:11 +0900

    [2024-05-29] 프로젝트 최초 생성

---
+++ Hanta_Plugin/Hanta.Plugin.Abstract/Plugin/IPlugin.cs
@@ -0,0 +1,33 @@
+﻿using Hanta.Plugin.Abstract.ViewModel;
+
+namespace Hanta.Plugin.Abstract.Plugin;
+/// <summary>
+/// 플러그인 인터페이스
+/// </summary>
+public interface IPlugin
+{
+	/// <summary>
+	/// 플러그인 아이콘 Kind속성 (MaterialDesign IconPack)
+	/// </summary>
+	string PluginIconKind { get; init; }
+
+	/// <summary>
+	/// 플러그인 메인 뷰모델
+	/// </summary>
+	IPluginViewModel PluginViewModel { get; init; }
+
+	/// <summary>
+	/// 플러그인 메인 뷰
+	/// </summary>
+	object PluginView { get; init; }
+
+	/// <summary>
+	/// 플러그인 설정 뷰 모델
+	/// </summary>
+	IPluginViewModel PluginConfigViewModel { get; init; }
+
+	/// <summary>
+	/// 플러그인 설정 뷰
+	/// </summary>
+	object PluginConfigView { get; init; }
+}
---
+++ Hanta_Plugin/Hanta.Plugin.Abstract/ViewModel/IPluginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Hanta.Plugin.Abstract.ViewModel;
+
+/// <summary>
+/// 플러그인 메인 뷰모델 인터페이스
+/// </summary>
+public interface IPluginViewModel
+{
+}
